bd3112ba0ca3362971ace41ca1dc30b4952d8e9e
devansh-dvj <devansh.dvj@gmail.com>
2020-05-02 14:56:56 +0530

    Indented shader_util.cpp

--- src/shader_util.cpp
+++ src/shader_util.cpp
@@ -1,87 +1,87 @@
 #include "road_network/shader_util.hpp"
 
-namespace soc
-{
-  GLuint LoadShaderGL(GLenum eShaderType, const std::string &strFilename)
-  {
-    std::ifstream shaderFile(strFilename.c_str());
-    if (!shaderFile.is_open())
-      throw std::runtime_error("Cannot find file: " + strFilename);
+namespace soc {
 
-    std::stringstream shaderData;
-    shaderData << shaderFile.rdbuf();
-    shaderFile.close();
+GLuint LoadShaderGL(GLenum eShaderType, const std::string &strFilename) {
+  std::ifstream shaderFile(strFilename.c_str());
+  if (!shaderFile.is_open())
+    throw std::runtime_error("Cannot find file: " + strFilename);
 
-    try
-      {
-	return CreateShaderGL(eShaderType, shaderData.str());
-      }
-    catch(std::exception &e)
-      {
-	std::cerr<<e.what()<<std::endl;
-	throw;
-      }
+  std::stringstream shaderData;
+  shaderData << shaderFile.rdbuf();
+  shaderFile.close();
+
+  try {
+    return CreateShaderGL(eShaderType, shaderData.str());
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    throw;
+  }
+}
+
+GLuint CreateShaderGL(GLenum eShaderType, const std::string &strShaderFile) {
+  GLuint shader = glCreateShader(eShaderType);
+  const char *strFileData = strShaderFile.c_str();
+  glShaderSource(shader, 1, &strFileData, NULL);
+
+  glCompileShader(shader);
+
+  GLint status;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+  if (status == GL_FALSE) {
+    GLint infoLogLength;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+    GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+    glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+
+    const char *strShaderType = NULL;
+
+    switch (eShaderType) {
+    case GL_VERTEX_SHADER:
+      strShaderType = "vertex";
+      break;
+    case GL_GEOMETRY_SHADER:
+      strShaderType = "geometry";
+      break;
+    case GL_FRAGMENT_SHADER:
+      strShaderType = "fragment";
+      break;
+    }
+
+    std::cerr << "Compile failure in " << strShaderType
+              << " shader:" << std::endl
+              << strInfoLog << std::endl;
+    delete[] strInfoLog;
   }
 
-  GLuint CreateShaderGL(GLenum eShaderType, const std::string &strShaderFile)
-  {
-    GLuint shader = glCreateShader(eShaderType);
-    const char *strFileData = strShaderFile.c_str();
-    glShaderSource(shader, 1, &strFileData, NULL);
+  return shader;
+}
 
-    glCompileShader(shader);
+GLuint CreateProgramGL(const std::vector<GLuint> &shaderList) {
+  GLuint program = glCreateProgram();
 
-    GLint status;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-    if (status == GL_FALSE)
-      {
-	GLint infoLogLength;
-	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+  for (size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
+    glAttachShader(program, shaderList[iLoop]);
 
-	GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-	glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+  glLinkProgram(program);
 
-	const char *strShaderType = NULL;
-	switch(eShaderType)
-	  {
-	  case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
-	  case GL_GEOMETRY_SHADER: strShaderType = "geometry"; break;
-	  case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
-	  }
+  GLint status;
+  glGetProgramiv(program, GL_LINK_STATUS, &status);
+  if (status == GL_FALSE) {
+    GLint infoLogLength;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
 
-	std::cerr<<"Compile failure in "<<strShaderType<<" shader:"<<std::endl<<strInfoLog<<std::endl;
-	delete[] strInfoLog;
-      }
-
-    return shader;
+    GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+    glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+    std::cerr << "GLSL Linker failure: " << strInfoLog << std::endl;
+    delete[] strInfoLog;
   }
 
-  GLuint CreateProgramGL(const std::vector<GLuint> &shaderList)
-  {
-    GLuint program = glCreateProgram();
+  for (size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
+    glDetachShader(program, shaderList[iLoop]);
 
-    for(size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
-      glAttachShader(program, shaderList[iLoop]);
+  return program;
+}
 
-    glLinkProgram(program);
-
-    GLint status;
-    glGetProgramiv (program, GL_LINK_STATUS, &status);
-    if (status == GL_FALSE)
-      {
-	GLint infoLogLength;
-	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
-
-	GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-	glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
-	std::cerr<<"GLSL Linker failure: "<<strInfoLog<<std::endl;
-	delete[] strInfoLog;
-      }
-
-    for(size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
-      glDetachShader(program, shaderList[iLoop]);
-
-    return program;
-  }
-
-};
+} // End namespace soc
