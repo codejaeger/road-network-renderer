096d8ebf9957cd845c86354fa8179c721c3ef27a
devansh-dvj <devansh.dvj@gmail.com>
2020-05-02 11:26:46 +0530

    Indented path.hpp

--- include/road_network/Bezier-Curve/path.hpp
+++ include/road_network/Bezier-Curve/path.hpp
@@ -4,13 +4,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <vector>
-#include <math.h>
 #include <glm/vec2.hpp>
+#include <math.h>
+#include <vector>
 
 #include "road_network/Road-Model/road.hpp"
 #include "road_network/Road-Model/road_sep.hpp"
 #include "road_network/shader_util.hpp"
+
 #ifdef _WIN32
 #include <Windows.h>
 #else
@@ -18,20 +19,25 @@
 #endif
 #define BZC 50
 
-namespace soc{
-	class Path{
-		std::vector<glm::vec2> positions;
-		glm::vec2 beizer_curve_positions[BZC + 1];
-		GLuint vb, vao;
-		GLuint shaderProgram;
-		GLuint v_position, uModelViewMatrix;
-		std::vector<glm::vec2> bezier_curve_point(std::vector<glm::vec2> pos, float ratio);
-	  public:
-	  	Path();
-	  	void getPoints(GLFWwindow *window);
-	  	void renderLine();
-			void save();
-	};
-}
+namespace soc {
+
+class Path {
+private:
+  std::vector<glm::vec2> positions;
+  glm::vec2 beizer_curve_positions[BZC + 1];
+  GLuint vb, vao;
+  GLuint shaderProgram;
+  GLuint v_position, uModelViewMatrix;
+  std::vector<glm::vec2> bezier_curve_point(std::vector<glm::vec2> pos,
+                                            float ratio);
+
+public:
+  Path();
+  void getPoints(GLFWwindow *window);
+  void renderLine();
+  void save();
+};
+
+} // End namespace soc
 
 #endif
