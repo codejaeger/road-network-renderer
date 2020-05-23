fe4bc35a9fa256af864aaaf4310407d4402669cc
amanyadav19 <amanyadaviitb@gmail.com>
2020-05-24 02:17:02 +0530

    commented graph.cpp hpp and initGraph function

--- src/Road-Graph/graph.cpp
+++ src/Road-Graph/graph.cpp
@@ -18,6 +18,7 @@ namespace soc {
 		int start_match=-1;
 		int end_match=-1;
 		bool a=false, b = false;
+		// check if the path has one endpoint as one of the vertices
 		for(int i=0; i<v.size(); i++){
 			if(calc_dist(path[0], v[i].origin)==0){
 				edge.from = i;
@@ -34,9 +35,11 @@ namespace soc {
 				break;
 			}
 		}
+		// if the start point is not any of the vertices then check it lies inside which intersection
 		if(edge.from == -1)
 			for(int i=0; i<v.size(); i++){
 				if(calc_dist(path[0], v[i].origin)<4*HALF_WIDTH){
+					// check if the endpoint is not that origin in case endpoint is a vertex and it is closer than 4d to it
 					if(path[path.size()-1]!=v[i].origin) {
 						edge.from = i;
 						start_match = i;
@@ -44,8 +47,10 @@ namespace soc {
 					}
 				}   
 			}
+		// if the end point is not any of the vertices then check it lies inside which intersection
 		if(edge.to == -1)
 			for(int i=0; i<v.size(); i++){
+				// check if the startpoint is not that origin in case startpoint is a vertex and it is closer than 4d to it
 				if(calc_dist(path[path.size()-1], v[i].origin)<4*HALF_WIDTH){
 					if(path[0]!=v[i].origin) {
 						edge.to = i;
@@ -54,6 +59,7 @@ namespace soc {
 					}
 				}
 			}
+		// if both of the endpoints are part of intersections
 		if(!(a || b)) {
 			edge.path = path;
 			float dist;
@@ -62,6 +68,7 @@ namespace soc {
 			}
 			edge.distance = dist;
 		}
+		// if only endpoints is a part of the intersection 
 		else if(a) {
 			path.erase(path.begin());
 			edge.path = path;
@@ -71,6 +78,7 @@ namespace soc {
 			}
 			edge.distance = dist; 
 		}
+		// if only start point is a part of the intersection
 		else if(b) {
 			path.erase(path.begin() + path.size() -1);
 			edge.path = path;
@@ -80,6 +88,7 @@ namespace soc {
 			}
 			edge.distance = dist;
 		}
+		// if both are not a part of intersection
 		else {
 			path.erase(path.begin() + path.size() -1);
 			path.erase(path.begin());
@@ -93,7 +102,7 @@ namespace soc {
 		e.push_back(edge);
 		v[start_match].outgoing.push_back(e.size()-1);
 		vertices[start_match].push_back(end_match);
-		// now the same things for reverse path
+		// now the same things for storing the reverse path since the edge is directed
 		Edge edge_rev;
 		edge_rev.distance = edge.distance;
 		edge_rev.from = edge.to;
@@ -109,6 +118,7 @@ namespace soc {
 		return sqrt((b[0]-a[0])*(b[0]-a[0]) + (b[1]-a[1])*(b[1]-a[1]));
 	}
 
+	// for explanation refer to djikstra algorithm of finding shortest path
 	std::vector<int> Graph::dijkstra(std::vector< std::vector<float> > G, int startnode, int endnode) {
 		int n = G.size();
 	  float cost[n][n],distance[n],pred[n];
@@ -157,6 +167,8 @@ namespace soc {
 	}
 	std::vector<int> Graph::getPath(int startnode, int endnode) {
 		std::vector< std::vector<float> > G(v.size());
+		// initialize the matrix to feed to the djikstra function where ijth element represent the distance between ith and jth node if they are neighbours
+		// and it is 0 if they are not connected directly
 		for(int i=0; i<v.size(); i++) {
 			for(int j=0; j<v.size(); j++) {
 				float dist=0;
@@ -168,7 +180,9 @@ namespace soc {
 			}
 		}
 		std::vector<int> path = dijkstra(G, startnode, endnode);
+		// the returned path is opposite to what we expect so reverse it
 		std::reverse(path.begin(), path.end());
+		// add the edges indices also in between the vertices
 		for(int i=path.size()-1; i>0; i--) {
 			for(int j=0; j<v[path[i-1]].outgoing.size(); j++) {
 					if(e[v[path[i-1]].outgoing[j]].to == path[i])
